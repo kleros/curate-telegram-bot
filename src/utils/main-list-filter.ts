40b772cb9a44143ebe48a23f8ec449519899e742
Green <40367733+greenlucid@users.noreply.github.com>
2022-11-25 18:33:48 -0300

    fix: mainlist filter stops crashing

--- src/utils/main-list-filter.ts
+++ src/utils/main-list-filter.ts
@@ -7,7 +7,7 @@ import config from "../config"
 // 2. if so, check if that list (take the 1st one) is also in a list.
 // 3. iterate 5 times.
 
-const getIncluderList = async (subgraphEndpoint: string, tcr: string) => {
+const getIncluderList = async (tcr: string) => {
   // not sure right now, if appears the data for both chain registries
   // starts with "0xd594". then, it predictably contains the address.
   const classicTargetData = `0xd594${tcr.slice(2)}`
@@ -29,20 +29,18 @@ const getIncluderList = async (subgraphEndpoint: string, tcr: string) => {
     `,
   }
 
-  const response = (await fetch(subgraphEndpoint, {
+  const response = (await fetch(config.SUBGRAPH_URL, {
     method: "POST",
     body: JSON.stringify(subgraphQuery),
   })) as any
 
+  const { data } = (await response.json()) as any
+
   const classicRegistry =
-    response.data.items.length > 0
-      ? response.data.items[0].registryAddress
-      : undefined
+    data.items.length > 0 ? data.items[0].registryAddress : undefined
 
   const lightRegistry =
-    response.data.itemProps > 0
-      ? response.data.itemProps[0].item.registryAddress
-      : undefined
+    data.itemProps > 0 ? data.itemProps[0].item.registryAddress : undefined
 
   if (classicRegistry) return classicRegistry
   else if (lightRegistry) return lightRegistry
@@ -50,17 +48,10 @@ const getIncluderList = async (subgraphEndpoint: string, tcr: string) => {
   return null
 }
 
-const containedInMain = async (
-  subgraphEndpoint: string,
-  main: string,
-  tcr: string
-) => {
+const containedInMain = async (main: string, tcr: string) => {
   let currentTcr = tcr.toLowerCase()
   for (let i = 0; i < 5; i++) {
-    const containerRegistry = await getIncluderList(
-      subgraphEndpoint,
-      currentTcr
-    )
+    const containerRegistry = await getIncluderList(currentTcr)
     if (containerRegistry === null) return false
     if (containerRegistry.toLowerCase() === main.toLowerCase()) return true
 
@@ -71,8 +62,6 @@ const containedInMain = async (
   // sorry i didn't bother to implement dijsktra
   return false
 }
-
-const fakeMainListFilter = async (tcr: string) => true
 
 /**
  * Checks if a tcr is contained in the main registry, or if it's the main registry.
@@ -85,9 +74,9 @@ const mainListFilter = async (tcr: string): Promise<boolean> => {
     return true
   }
 
-  const result = await containedInMain(config.SUBGRAPH_URL, main, tcr)
+  const result = await containedInMain(main, tcr)
   console.log(`tcr: ${tcr} went through filter. Result: ${result}`)
   return result
 }
 
-export default fakeMainListFilter
+export default mainListFilter
