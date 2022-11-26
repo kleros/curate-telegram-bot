80187fb302a2405f2dd5adeffd601a15fe64353b
Green <40367733+greenlucid@users.noreply.github.com>
2022-11-26 15:35:03 -0300

    fix: metaevidence last resort

--- src/utils/get-meta-evidence.ts
+++ src/utils/get-meta-evidence.ts
@@ -1,28 +1,41 @@
+import { ethers } from "ethers"
 import fetch from "node-fetch"
 import config from "../config"
 import { CurateEvent } from "../get-events"
+import CLASSIC_ABI from "../abis/GeneralizedTCR.json"
+import LIGHT_ABI from "../abis/LightGeneralizedTCR.json"
 
 interface ProcessedMetaEvidence {
   itemName: string
   tcrTitle: string
 }
 
+const rpcMetaEvidenceFetchURI = async (event: CurateEvent): Promise<string> => {
+  const provider = new ethers.providers.JsonRpcProvider(config.RPC)
+  const abi = event.version === "classic" ? CLASSIC_ABI : LIGHT_ABI
+  const tcr = new ethers.Contract(event.tcrAddress, abi, provider)
+  const logs = (
+    await provider.getLogs({
+      ...tcr.filters.MetaEvidence(),
+      fromBlock: 0,
+    })
+  ).map((log) => tcr.interface.parseLog(log))
+  const { _evidence: metaEvidencePath } = logs[logs.length - 1].args
+  return metaEvidencePath
+}
+
 const getMetaEvidence = async (
   event: CurateEvent
 ): Promise<ProcessedMetaEvidence | null> => {
   let tcrMetaEvidence: any
-  const uri = event.details.registrationMetaEvidenceURI
+  let uri = event.details.registrationMetaEvidenceURI
   if (!uri) {
-    console.warn("Attempted to fetch metaevidence, but no uri", event)
-    return null
+    console.warn("(meta) No uri in subgraph, fetching manually...")
+    uri = await rpcMetaEvidenceFetchURI(event)
   }
+
   try {
-    tcrMetaEvidence = await (
-      await fetch(
-        config.IPFS_GATEWAY +
-          (event.details.registrationMetaEvidenceURI as string)
-      )
-    ).json()
+    tcrMetaEvidence = await (await fetch(config.IPFS_GATEWAY + uri)).json()
   } catch (err) {
     console.warn("Error fetching meta evidence", event)
     return null
